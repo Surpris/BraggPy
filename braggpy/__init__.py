7dcd1d7585a3a9aefd8d1a07a22257b4313181df
Toshiyuki NH <take90-it09-easy27@outlook.jp>
2023-08-10 09:21:09 +0000

    edit __init__

--- braggpy/__init__.py
+++ braggpy/__init__.py
@@ -8,6 +8,9 @@ from .in_out_shape import (
     is_inside, is_inside_index,
     is_inside_sphere, is_inside_sphere_index
 )
-from .mathfunctions import make_lattice_points, euler_rotate, calc_euler_hkl
+from .mathfunctions import (
+    make_lattice_points, euler_rotate, calc_euler_hkl,
+    calculate_lattice_distance
+)
 from .momentum import generate_momentum, generate_momentum_polar
 from .utils import elapsed
