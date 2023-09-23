9851f684ddeb40eaf09180e697b4f19ee75d7bea
Toshiyuki NH <take90-it09-easy27@outlook.jp>
2023-09-23 07:00:27 +0000

    add a function with concurrent.futures

--- braggpy/calcfunctions.py
+++ braggpy/calcfunctions.py
@@ -3,6 +3,7 @@
 functions to calculate modulus
 """
 
+import concurrent.futures
 import numpy
 
 
@@ -13,7 +14,42 @@ def calc_modulus(
     qzz: numpy.ndarray
 ) -> numpy.ndarray:
     """
-    Calculate modulus
+    Calculate a modulus
+
+    Parameters
+    ----------
+    coors : numpy.ndarrry
+        spatial coordinates (positions of atoms).
+        coors has a shape of (N, 3), where N is
+        the number of atoms in the target crystal.
+    qxx : numpy.ndarrry
+        momentum coordinates in the X axis.
+    qyy : numpy.ndarrry
+        momentum coordinates in the Y axis.
+    qzz : numpy.ndarrry
+        momentum coordinates in the Z axis (same as the incident X-ray beam).
+
+    Returns
+    -------
+    numpy.ndarray : the Fourier modulus.
+    """
+
+    dst = numpy.zeros(qxx.shape, dtype=complex)
+    for coor in coors:
+        phase = coor[0] * qxx + coor[1] * qyy + coor[2] * qzz
+        dst += numpy.exp(-1j*phase)
+    return dst
+
+
+def calc_modulus_mulptiroccess(
+    coors: numpy.ndarray,
+    qxx: numpy.ndarray,
+    qyy: numpy.ndarray,
+    qzz: numpy.ndarray,
+    n_workers: int = 4
+) -> numpy.ndarray:
+    """
+    Calculate a modulus with multi processing.
 
     Parameters
     ----------
@@ -28,9 +64,20 @@ def calc_modulus(
     qzz : numpy.ndarrry
         momentum coordinates in the Z axis (same as the incident X-ray beam).
     """
-
-    F = numpy.zeros(qxx.shape, dtype=complex)
-    for coor in coors:
-        phase = coor[0] * qxx + coor[1] * qyy + coor[2] * qzz
-        F += numpy.exp(-1j*phase)
-    return F
+    n_coors = len(coors)
+    n_coors_per_worker = n_coors // n_workers
+    dst = numpy.zeros(qxx.shape, dtype=complex)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=n_workers) as executor:
+        futures = []
+        for index in range(n_workers - 1):
+            futures.append(executor.submit(
+                n_coors[index * n_coors_per_worker:(index + 1) * n_coors_per_worker],
+                qxx, qyy, qzz
+            ))
+        futures.append(executor.submit(
+            n_coors[(n_workers - 1) * n_coors_per_worker:],
+            qxx, qyy, qzz
+        ))
+        for future_ in concurrent.futures.as_completed(futures):
+            dst += future_.result()
+    return dst
